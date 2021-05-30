86da102d432d26bc52dab0dc6057695ed1b583d4
Helmi-Simplon <shan.tunisois@gmail.com>
2021-05-30 20:49:20 +0200

    add bootstrap to validationForm.php

--- view/validationForm.php
+++ view/validationForm.php
@@ -1,3 +1,10 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Questionnaire de satisfaction Amazin</title>
+</head>
+<div class="container" style="font-weight:bold;color:gray;font-family:Roboto;">
+
 <p>Merci pour votre pour votre notation :</p>
 <p><?= star().circle() ?></p>
 <?php
@@ -6,3 +13,5 @@ $q3 = isset($_POST['q3']) ? $_POST['q3'] : NULL;
 if($case && $q3 == -1){ ?>
 <p>Vous serez rappelé au : <?= phone() ?></p>
 <?php } ?>
+<a href="index.php" role="button" class="btn btn-outline-secondary">Retour à la page d'accueil</a>
+</div>
